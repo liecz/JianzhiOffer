4e7bfe4fa25f82f8806cd8eaffa8c12f2c33a27b
李悦 <554196606@qq.com>
2017-02-13 19:37:20 +0800

    Q.顺时针打印矩阵. Q.二叉树的镜像.

--- src/com/solutions/Solution_BTreeMirror.java
+++ src/com/solutions/Solution_BTreeMirror.java
@@ -2,11 +2,41 @@ package com.solutions;
 
 import com.util.TreeNode;
 
+
 /**
  * Created by liec on 2017-02-13.
  */
 public class Solution_BTreeMirror {
     public void Mirror(TreeNode root) {
-
+        if(root != null){
+            TreeNode tmp;
+            //switching two sub-tree
+            tmp = root.left;
+            root.left = root.right;
+            root.right = tmp;
+            //recursive
+            if(root.left!=null)
+                Mirror(root.left);
+            if(root.right!=null)
+                Mirror(root.right);
+        }
+    }
+    public void Mirror_1(TreeNode root) {
+        if(root==null)
+            return;
+        java.util.Deque<TreeNode> s = new java.util.ArrayDeque<>();
+        s.push(root);
+        while(!s.isEmpty()){
+            TreeNode r = s.pop();
+            TreeNode tmp;
+            //switching two sub-tree
+            tmp = r.left;
+            r.left = r.right;
+            r.right = tmp;
+            if(r.left!=null)
+                s.push(r.left);
+            if(r.right!=null)
+                s.push(r.right);
+        }
     }
 }
