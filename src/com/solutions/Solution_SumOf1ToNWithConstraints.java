63a72640a4fad23156b8a9a1c9e4167002429c6e
李悦 <554196606@qq.com>
2017-03-05 11:36:19 +0800

    Q.求1+2+3+...+n.

---
+++ src/com/solutions/Solution_SumOf1ToNWithConstraints.java
@@ -0,0 +1,22 @@
+package com.solutions;
+
+/**
+ * 求1+2+3+...+n
+ * 求1+2+3+...+n，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
+ * Created by liec on 2017-03-05.
+ */
+public class Solution_SumOf1ToNWithConstraints {
+    public static void main(String[] args) {
+        Solution_SumOf1ToNWithConstraints s = new Solution_SumOf1ToNWithConstraints();
+        System.out.println(s.Sum_Solution(5));
+    }
+
+    /*
+    * 利用短路机制控制语句执行
+    * */
+    public int Sum_Solution(int n) {
+        int ans = n;
+        boolean b = ans != 0 && (ans += Sum_Solution(n - 1)) > 0;
+        return ans;
+    }
+}
